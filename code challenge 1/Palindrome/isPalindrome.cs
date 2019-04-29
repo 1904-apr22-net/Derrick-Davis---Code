de9afc9dbe7ff0fa961dbec2dadcdd46946ddc7c
Derrick Davis <derrdavis@gmail.com>
2019-04-29 12:04:17 -0500

    program challenge

---
+++ code challenge 1/Palindrome/isPalindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace Palindrome
+{
+    public class isPalindrome
+    {
+        public string x, rev;
+        public int leng = 0;
+        public  InfoInput()
+        {
+            
+            Console.WriteLine("Enter a string");
+            String a = Console.ReadLine();
+            leng = x.Length - 1;
+            while (leng >= 0) {
+                x = a;
+                rev = rev + x[leng];
+                leng--;
+           }
+
+            bool IsEqlPalinD()
+            {
+                if (x == rev)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+              
+        }
+    }
+}
